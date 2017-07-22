8733fa7f32045876ea1763c89a0aca35bb2c6840
Matt Hughes <matthughes.tech@gmail.com>
2017-07-21 22:37:54 -0400

    Write API response to serial console

--- subscriberCount.ino
+++ subscriberCount.ino
@@ -11,13 +11,52 @@ char password[] = "";
 
 String channel = "";
 String apiKey = "";
-String url = "https://content.googleapis.com/youtube/v3/channels?id=" + channel + "&part=statistics&key=" + apiKey;
+String resource = "/youtube/v3/channels?id=" + channel + "&part=statistics&key=" + apiKey;
+const char host[] = "content.googleapis.com";
+const char* fingerprint = "ef d7 9c 64 94 7c 7e c4 7b d8 6f b3 0b 0a c4 b6 7a 97 7d f9";
 
 Adafruit_7segment matrix = Adafruit_7segment();
 WiFiClientSecure client;
 
+void setup() {
+  Serial.begin(115200);
+
+  matrix.begin(0x70);
+  matrix.print(100000, DEC);
+  matrix.writeDisplay();
+}
+
+void loop() {
+  if (WiFi.status() != WL_CONNECTED) {
+    connectToWifi();
+  }
+
+  if (!client.connect(host, 443)) {
+    Serial.println("Could not connect to server");
+    return;
+  }
+
+  if (!client.verify(fingerprint, host)) {
+    Serial.println("Unexpected SSL certificate");
+    return;
+  }
+
+  Serial.print("GET ");
+  Serial.println(resource);
+
+  String request = "GET " + resource + " HTTP/1.1\r\n" + "Host: " + host + "\r\n" + "Connection: close\r\n\r\n";
+  client.print(request);
+
+  while (client.connected()) {
+    String line = client.readStringUntil('\n');
+    Serial.println(line);
+  }
+
+  // wait 60 seconds
+  delay(1000 * 60);
+}
+
 void connectToWifi() {
-  // Attempt to connect to Wifi network:
   Serial.println();
   Serial.print("Connecting to network: ");
   Serial.print(ssid);
@@ -31,23 +70,4 @@ void connectToWifi() {
   Serial.println("IP address: ");
   IPAddress ip = WiFi.localIP();
   Serial.println(ip);
-}
-
-void setup() { 
-  Serial.begin(115200);  
-  delay(1000);
-  
-  matrix.begin(0x70);
-  matrix.print(100000, DEC);
-  matrix.writeDisplay();
-
-  //connectToWifi();
-}
-
-void loop() {
-  if (WiFi.status() != WL_CONNECTED) {
-    connectToWifi();
-  }
-        
-  delay(1000);
 }
